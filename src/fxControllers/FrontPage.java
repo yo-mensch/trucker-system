a47c0d4e540bad3b0baa55f1345ba7ea1a8348f5
Deividas Sitdykov <deividas.sitdykov@gmail.com>
2023-04-03 17:52:12 +0300

    Added trucker editYourData feature

--- src/fxControllers/FrontPage.java
+++ src/fxControllers/FrontPage.java
@@ -169,6 +169,22 @@ public class FrontPage {
         truckerList.getItems().clear();
         fillList();
     }
+
+    public void editYourData() throws IOException {
+        if (user.getClass() == Trucker.class) {
+            FXMLLoader fxmlLoader = new FXMLLoader(UpdateTrucker.class.getResource("../fxml/update-trucker-page.fxml"));
+            Parent parent = fxmlLoader.load();
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage();
+            stage.setTitle("Update");
+            stage.setScene(scene);
+            stage.show();
+            UpdateTrucker updateTrucker = fxmlLoader.getController();
+            updateTrucker.setData(entityManagerFactory, trucker);
+            truckerList.getItems().clear();
+            fillList();
+        }
+    }
     //------------------FORUM----------------------
     public void deleteForum() {
         forumHib.deleteForum(forumHib.getForumById(forumList.getSelectionModel().getSelectedItem().getId()));
